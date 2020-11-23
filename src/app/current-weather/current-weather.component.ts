568199f493743e994c130b016b803fb006680171
Michael Petry <2michael.petry@gmail.com>
2020-11-22 21:26:14 -0500

    Display current location weather information for the current day
    Implement displaying current location weather information for the current day

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core'
+
+import { ICurrentWeather } from '../interfaces'
+import { WeatherService } from '../weather/weather.service'
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css'],
+})
+export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit(): void {
+    this.weatherService
+      .getCurrentWeather('Toronto', 'CA')
+      .subscribe((data) => (this.current = data))
+  }
+}
